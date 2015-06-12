e0d43fb81dc43196b5053cc01252d5579c168db0
Pedro Ferreira <plferreira4@gmail.com>
2015-06-12 12:33:24 +0100

    - add missing encoding in index.html - fix, use clientX/clientY instead of offsetX/offsetY (to work in firefox)

--- scripts/game.js
+++ scripts/game.js
@@ -270,8 +270,8 @@ if ( IS_PAUSED && !BEFORE_FIRST_WAVE )
     }
 
 var button = event.button;
-var x = event.offsetX;
-var y = event.offsetY;
+var x = event.clientX;
+var y = event.clientY;
 var a;
 var tower;
 var point;
