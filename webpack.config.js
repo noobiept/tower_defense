5dc7129b247973c3364d5faeba790c5f536128ab
Pedro Ferreira <plferreira4@gmail.com>
2022-06-03 02:32:39 +0200

    CHANGE: Start converting the project to typescript.
    - Define nodejs version used.
    - Add initial typescript configuration.
    - Add initial webpack configuration.

---
+++ webpack.config.js
@@ -0,0 +1,43 @@
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const CopyPlugin = require("copy-webpack-plugin");
+const Path = require("path");
+const Package = require("./package.json");
+
+module.exports = {
+    entry: "./scripts/index.js",
+    module: {
+        rules: [
+            {
+                test: /\.tsx?$/,
+                use: "ts-loader",
+                exclude: /node_modules/,
+            },
+        ],
+    },
+    resolve: {
+        extensions: [".tsx", ".ts", ".js"],
+    },
+    output: {
+        filename: "bundle.js",
+        path: Path.resolve(
+            __dirname,
+            "release",
+            `${Package.name} ${Package.version}`
+        ),
+        clean: true,
+    },
+    plugins: [
+        new CopyPlugin({
+            patterns: [
+                { from: "libraries", to: "libraries" },
+                { from: "scripts/**/*.js", to: "./" },
+                { from: "css", to: "css" },
+                { from: "maps", to: "maps" },
+                { from: "images/*.png", to: "./" },
+            ],
+        }),
+        new HtmlWebpackPlugin({
+            template: "./index.html",
+        }),
+    ],
+};
